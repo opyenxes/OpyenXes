4e9c0e49daba4344eeee68889cee5a4783f41112
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2017-11-08 17:15:47 -0300

    Modified info

--- opyenxes/__init__.py
+++ opyenxes/__init__.py
@@ -2,6 +2,6 @@
 
 """Top-level package for OpyenXes."""
 
-__author__ = """Wai Lam Jonathan Lee"""
-__email__ = 'jonathan.wailam.lee@gmail.com'
+__author__ = """Process Mining UC"""
+__email__ = 'processmininguc@gmail.com'
 __version__ = '0.1.1'
