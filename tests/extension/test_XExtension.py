d3f7d35b1b3e342388cc2fa5063e1017cf39cdc2
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2017-12-01 14:54:20 -0300

    Added some simple tests for XExtension

---
+++ tests/extension/test_XExtension.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""This is the test module for XExtension.
+
+This module tests XExtension.
+"""
+
+from opyenxes.extension import *
+from urllib.parse import urlencode
+import pytest
+
+
+__author__ = "Wai Lam Jonathan Lee"
+__email__ = "walee@uc.cl"
+
+
+@pytest.fixture()
+def extension():
+    name = 'ext0'
+    prefix = 'ext'
+    uri = urlencode({'uri': 'value'})
+    extension = XExtension(name, prefix, uri)
+    return extension
+
+
+def test_create_extension():
+    name = 'ext0'
+    prefix = 'ext'
+    uri = urlencode({'arg': 'value'})
+    extension = XExtension(name, prefix, uri)
+    assert extension is not None
+    assert extension.get_uri() == uri
+    assert extension.get_name() == name
+    assert extension.get_prefix() == prefix
+
+
+def test_get_defined_attributes(extension):
+    defined_attrs = extension.get_defined_attributes()
+    assert len(defined_attrs) == 0
