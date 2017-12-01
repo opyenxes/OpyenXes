813b1f85cc0df933289e0cf3c22c1b348c795ac4
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2017-12-01 14:40:31 -0300

    Make XExtension class discoverable

--- opyenxes/extension/XExtension.py
+++ opyenxes/extension/XExtension.py
@@ -1,3 +1,6 @@
+__all__ = ['XExtension']
+
+
 class XExtension:
     """This class defines and implements extensions to the basic log meta-model.
     Extensions have a name, a defined prefix, and a unique URI. They can define
---
+++ opyenxes/extension/__init__.py
@@ -0,0 +1,1 @@
+from .XExtension import *
